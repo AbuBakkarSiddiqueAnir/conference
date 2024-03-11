9c4d5a7a2aa97a451dde98c3292ad1ed78cac6c0
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2024-03-12 00:14:54 +0600

    conferences refactoring desing :tada:

--- app/(home)/_components/conferences.tsx
+++ app/(home)/_components/conferences.tsx
@@ -12,23 +12,23 @@ type Props = {
 
 export default function Conferences({ conferences }: Props) {
   return (
-    <section className="w-full  mt-32">
+    <section className="w-full  mt-32 pb-10 md:pb-24">
       <h2 className="text-center text-h-large mb-8">Conferences</h2>
       <div className="max-w-[70rem] mx-auto">
-        <div className="timeline space-y-[60px]">
+        <div className="timeline relative w-full max-w-[1140px]  py-4 px-0 space-y-[60px]">
           {conferences.map((conference, i) => {
             return (
               <div
                 key={i}
                 className={clsx(
-                  "container-x rounded-md ",
+                  "container-timeline px-[30px] relative bg-inherit w-1/2 py-4 rounded-md ",
                   i % 2 === 0 ? "left" : "right"
                 )}
               >
                 <div className="date text-gray">
                   {formatDate(conference.startDate)}
                 </div>
-                <div className="content shadow-extended  drop-shadow-extended rounded-md bg-[#F9FAFB] flex gap-4">
+                <div className="content p-2 md:p-7 shadow-extended  drop-shadow-extended rounded-md bg-[#F9FAFB] flex gap-4">
                   <div>
                     <Image
                       src={"/Marker.svg"}
