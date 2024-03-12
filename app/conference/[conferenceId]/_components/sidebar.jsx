231aeecd658f57e798597a2fb7f175b5e8c19e9f
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2024-03-12 14:58:17 +0600

    sortables tried

--- app/conference/[conferenceId]/_components/sidebar.jsx
+++ app/conference/[conferenceId]/_components/sidebar.jsx
@@ -11,76 +11,79 @@ import { TweenLite } from "./sortable";
 const Sidebar = ({ cf, handleCurrentTab, currentTab }) => {
   const [sortables, setSortables] = useState(cf);
   const containerRef = useRef(null);
-  const changeIndex = (index, to, elementRef) => {
-    const listItems = Array.from(document.querySelectorAll(".list-item"));
-    arrayMove(listItems, index, to);
-    if (to === sortables.length - 1) {
-      containerRef.current.appendChild(elementRef.current);
-    } else {
-      const i = index > to ? to : to + 1;
-      console.log(containerRef);
 
-      containerRef.current.insertBefore(elementRef.current, listItems[i]);
-    }
-  };
-  const arrayMove = (array, from, to) => {
+  const arrayMove = (from, to) => {
+    let array = [...sortables];
     array.splice(to, 0, array.splice(from, 1)[0]);
-    // setSortables(arr);
+    console.log(array);
+    setSortables(array);
   };
   // eslint-disable-next-line react/display-name
-  const Draggable = forwardRef(({ children, dragAction, start, end }, ref) => {
-    useEffect(() => {
-      const onMouseDown = (event) => {
-        event.preventDefault();
-        let startY = event.clientY;
-
-        const onMouseMove = (event) => {
-          if (!ref.current) return;
+  const Draggable = forwardRef(
+    ({ children, dragAction, start, end, index, rowSize, layout }, ref) => {
+      useEffect(() => {
+        const onMouseDown = (event) => {
           event.preventDefault();
+          let startY = event.clientY;
 
-          const deltaY = event.clientY - startY;
-          const newPosition = ref.current.offsetTop + deltaY;
-          // console.log(ref.current.parentElement);
-          const maxY = Math.round(
-            ref.current.parentElement.offsetHeight - ref.current.offsetHeight
-          );
+          const onMouseMove = (event) => {
+            if (!ref.current) return;
+            event.preventDefault();
 
-          const newY = Math.max(0, Math.min(newPosition, maxY));
+            const deltaY = event.clientY - startY;
+            const newPosition = ref.current.offsetTop + deltaY;
 
-          ref.current.style.top = `${newY}px`;
-          dragAction();
-          startY = event.clientY;
-        };
+            // console.log(deltaY, startY);
+            const maxY =
+              ref.current.parentElement.offsetHeight - ref.current.offsetHeight;
 
-        const onMouseUp = () => {
-          if (!ref.current) return;
+            const newY = Math.max(0, Math.min(newPosition, maxY));
 
-          document.removeEventListener("mousemove", onMouseMove);
-          document.removeEventListener("mouseup", onMouseUp);
-          for (let prop in end) {
-            ref.current.style[prop] = end[prop];
+            ref.current.style.top = `${newY}px`;
+
+            startY = event.clientY;
+
+            layout();
+          };
+
+          const onMouseUp = () => {
+            if (!ref.current) return;
+
+            containerRef.current.removeEventListener("mousemove", onMouseMove);
+            containerRef.current.removeEventListener("mouseup", onMouseUp);
+            for (let prop in end) {
+              ref.current.style[prop] = end[prop];
+            }
+          };
+
+          containerRef.current.addEventListener("mousemove", onMouseMove);
+          containerRef.current.addEventListener("mouseup", onMouseUp);
+
+          for (let prop in start) {
+            ref.current.style[prop] = start[prop];
           }
         };
+        const onMouseUpELement = (event) => {
+          if (!ref.current) return;
+          const newIndex = Math.round(ref.current.offsetTop / rowSize);
+          if (newIndex !== index) {
+            arrayMove(index, newIndex);
+          }
+        };
+        ref.current.addEventListener("mousedown", onMouseDown);
+        ref.current.addEventListener("mouseup", onMouseUpELement);
+      }, [ref]);
 
-        document.addEventListener("mousemove", onMouseMove);
-        document.addEventListener("mouseup", onMouseUp);
-
-        for (let prop in start) {
-          ref.current.style[prop] = start[prop];
-        }
-      };
-      ref.current.addEventListener("mousedown", onMouseDown);
-    }, [ref]);
-
-    return (
-      <li
-        ref={ref}
-        className="h-[72px] absolute list-item border border-[#D9D9D9] text-body-1 rounded-sm"
-      >
-        {children}
-      </li>
-    );
-  });
+      return (
+        <li
+          ref={ref}
+          className="h-[72px] absolute list-item border border-[#D9D9D9] text-body-1 rounded-sm"
+        >
+          {children}
+        </li>
+      );
+    }
+  );
 
   const Sortable = ({ index, rowSize, conference }) => {
     const elementRef = useRef(null);
@@ -95,13 +98,10 @@ const Sidebar = ({ cf, handleCurrentTab, currentTab }) => {
 
     return (
       <Draggable
-        dragAction={() => {
-          const newIndex = Math.round(elementRef.current.offsetTop / rowSize);
-          if (newIndex !== index) {
-            changeIndex(index, newIndex, elementRef);
-          }
-        }}
         ref={elementRef}
+        layout={layout}
+        index={index}
+        rowSize={rowSize}
         start={{
           boxShadow: "rgba(0, 0, 0, 0.2) 0px 16px 32px 0px",
           zIndex: 100,
@@ -134,12 +134,12 @@ const Sidebar = ({ cf, handleCurrentTab, currentTab }) => {
         ref={containerRef}
         className="w-full flex flex-col gap-y-6 h-[384px] relative"
       >
-        {cf.map((conference, index) => (
+        {sortables.map((conference, index) => (
           <Sortable
-            key={index}
+            key={conference.id}
             conference={conference}
             index={index}
-            rowSize={90}
+            rowSize={100}
           />
         ))}
       </ul>
